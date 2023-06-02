ef83e1be7ece34221a2180ebbbd9451a1421aa8e
Guilherme de Souza Ferreira <guilherme.ea16013@gmail.com>
2023-06-02 00:33:33 -0300

    Terminando o resto do CRUD

--- locadoracrud/src/main/java/br/com/senac/locadoracrud/controller/CarroController.java
+++ locadoracrud/src/main/java/br/com/senac/locadoracrud/controller/CarroController.java
@@ -2,12 +2,17 @@ package br.com.senac.locadoracrud.controller;
 
 import br.com.senac.locadoracrud.data.CarroRepository;
 import br.com.senac.locadoracrud.model.Carro;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/carros")
@@ -47,4 +52,30 @@ public class CarroController {
     public String handleError() {
         return "error";
     }
+
+    @GetMapping("/alterarCarro")
+public String exibirFormularioAlterarCarro(Model model, @RequestParam("id") Long id) {
+    Optional<Carro> optionalCarro = carroRepository.findById(id);
+    if (optionalCarro.isPresent()) {
+        Carro carro = optionalCarro.get();
+        model.addAttribute("carro", carro);
+        return "formulario-alterar-carro";
+    } else {
+        return "error"; // Página de erro caso o carro não seja encontrado
+    }
+}
+
+@PostMapping("/alterarCarro")
+public String alterarCarro(@ModelAttribute("carro") Carro carro) {
+    carroRepository.save(carro);
+    return "redirect:/carros/listar";
+}
+
+@GetMapping("/excluirCarro")
+public String excluirCarro(@RequestParam("id") Long id) {
+    carroRepository.deleteById(id);
+    return "redirect:/carros/listar";
+}
+
+
 }
